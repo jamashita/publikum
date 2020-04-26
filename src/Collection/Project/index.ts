05fd49c6d0e5c8c561b36ffbda91c7cd8c733010
jamashita <jamashita@arachnid.xyz>
2020-04-27 00:29:02 +0900

    Project

---
+++ src/Collection/Project/index.ts
@@ -0,0 +1,4 @@
+export * from './Abstract/AProject';
+export * from './Interface/Project';
+export * from './Mock/MockAProject';
+export * from './ImmutableProject';
--- src/Collection/index.ts
+++ src/Collection/index.ts
@@ -1,3 +1,4 @@
 export * from './Address';
 export * from './Interface/Collection';
+export * from './Project';
 export * from './Sequence';
