214772229de157176fe41090b7537de284463baa
jamashita <jamashita@arachnid.xyz>
2020-05-24 14:36:47 +0900

    type

---
+++ packages/type/jest.config.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  verbose: true,
+  bail: true,
+  roots: [
+    'src'
+  ],
+  testRegex: '/__tests__/.+\\.spec\\.tsx?$',
+  testPathIgnorePatterns: [
+    '/node_modules/'
+  ],
+  collectCoverage: true,
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/Mock/'
+  ],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'ts',
+    'tsx',
+    'json',
+    'node'
+  ],
+  testURL: 'http://localhost'
+};
