a2b9131638c2ce475bdf97ea344985ef158b9b96
jamashita <jamashita@arachnid.xyz>
2020-05-24 16:53:19 +0900

    wip

--- packages/type/src/Value.ts
+++ packages/type/src/Value.ts
@@ -3,7 +3,7 @@ export type Nullable<T> = T | null;
 export type Ambiguous<T> = T | undefined;
 export type Suspicious<T> = T | null | undefined;
 export type PlainObject = {
-  [name: string]: Item | Array<Item>;
+  [key: string]: Item | Array<Item>;
 };
 type Item = Primitive | PlainObject;
 export type JSObjectNotation = PlainObject | Array<Item>;
