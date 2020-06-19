1d9a43b1240f31b619ea3b4a93673f958e42555b
jamashita <jamashita@arachnid.xyz>
2020-06-20 03:21:17 +0900

    Still

--- packages/monad/src/Superposition/Still.ts
+++ packages/monad/src/Superposition/Still.ts
@@ -1,5 +1,3 @@
-import { Predicate, UnaryFunction } from '@jamashita/publikum-type';
-
 import { Alive } from './Alive';
 import { Dead } from './Dead';
 import { SuperpositionError } from './Error/SuperpositionError';
@@ -30,32 +28,13 @@ export class Still<S, F extends Error> implements Schrodinger<S, F, 'Still'> {
     return false;
   }
 
-  public filter(predicate: Predicate<S>): never;
-  public filter(): never {
-    throw new SuperpositionError('STILL');
-  }
-
-  public map<T, E extends Error>(mapper: UnaryFunction<S, Schrodinger<T, E>>): never;
-  public map<T, E extends Error = F>(mapper: UnaryFunction<S, T>): never;
-  public map<T, E extends Error = F>(mapper: UnaryFunction<S, Promise<T>>): never;
-  public map<T, E extends Error = F>(): never {
-    throw new SuperpositionError('STILL');
-  }
-
-  public recover<T, E extends Error>(mapper: UnaryFunction<F, Schrodinger<T, E>>): never;
-  public recover<T, E extends Error = F>(mapper: UnaryFunction<F, T>): never;
-  public recover<T, E extends Error = F>(mapper: UnaryFunction<F, Promise<T>>): never;
-  public recover<T, E extends Error = F>(): never {
-    throw new SuperpositionError('STILL');
-  }
-
   public transpose<T, E extends Error>(): Still<T, E> {
     return (this as unknown) as Still<T, E>;
   }
 
   /*
-   * public toQuantum(): Quantum<S> {
-   *   return Absent.of<S>();
+   * public toQuantum(): never {
+   *   throw new SuperpositionError('STILL');
    * }
    */
 }
---
+++ packages/monad/src/Superposition/__tests__/Still.spec.ts
@@ -0,0 +1,55 @@
+import { MockError } from '@jamashita/publikum-object';
+
+import { SuperpositionError } from '../Error/SuperpositionError';
+import { Still } from '../Still';
+
+describe('Still', () => {
+  describe('of', () => {
+    it('returns a single instance', () => {
+      expect(Still.of<number, MockError>()).toBe(Still.of<number, MockError>());
+    });
+  });
+
+  describe('get', () => {
+    it('throws the inside error', () => {
+      const still: Still<number, MockError> = Still.of<number, MockError>();
+
+      expect(() => {
+        still.get();
+      }).toThrow(SuperpositionError);
+    });
+  });
+
+  describe('isAlive', () => {
+    it('always returns false', () => {
+      const still: Still<number, MockError> = Still.of<number, MockError>();
+
+      expect(still.isAlive()).toBe(false);
+    });
+  });
+
+  describe('isDead', () => {
+    it('always returns false', () => {
+      const still: Still<number, MockError> = Still.of<number, MockError>();
+
+      expect(still.isDead()).toBe(false);
+    });
+  });
+
+  /*
+   * describe('toQuantum', () => {
+   *   it('returns Present', () => {
+   *     const alive: Still<number, MockError> = Still.of<number, MockError>(100);
+   *     expect(alive.toQuantum()).toBeInstanceOf(Present);
+   *   });
+   * });
+   */
+
+  describe('transpose', () => {
+    it('returns itself', () => {
+      const still: Still<number, MockError> = Still.of<number, MockError>();
+
+      expect(still.transpose<void, MockError>()).toBe(still);
+    });
+  });
+});
