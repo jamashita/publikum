96e26f047964f4ac62b3a6d9f3988980dace520d
jamashita <jamashita@arachnid.xyz>
2020-06-22 14:02:42 +0900

    comingh

--- packages/type/src/Function.ts
+++ packages/type/src/Function.ts
@@ -1,9 +1,9 @@
 export type UnaryFunction<I, O> = (arg: I) => O;
 export type BinaryFunction<I1, I2, O> = (arg1: I1, arg2: I2) => O;
-export type Consumer<T> = (arg: T) => void | unknown;
-export type AsyncConsumer<T> = (arg: T) => Promise<void | unknown>;
 export type Predicate<T> = (arg: T) => boolean;
 export type BinaryPredicate<T1, T2> = (arg1: T1, args2: T2) => boolean;
+export type Consumer<T> = (arg: T) => void | unknown;
+export type AsyncConsumer<T> = (arg: T) => Promise<void | unknown>;
 export type Supplier<T> = () => T;
 export type AsyncSupplier<T> = () => Promise<T>;
 export type Peek = () => unknown;
