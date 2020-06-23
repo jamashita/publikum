ef6781c62ac6be629f149a5caa336366ca338a3a
jamashita <jamashita@arachnid.xyz>
2020-06-24 00:02:11 +0900

    practical executros

--- packages/monad/src/Superposition/MapExecutor.ts
+++ packages/monad/src/Superposition/MapExecutor.ts
@@ -1,29 +1,28 @@
 import { Reject, Resolve, UnaryFunction } from '@jamashita/publikum-type';
 
 import { AliveExecutor } from './AliveExecutor';
-import { DeadExecutor } from './DeadExecutor';
+import { DeadNothingExecutor } from './DeadNothingExecutor';
 import { IDeadOrAliveExecutor } from './Interface/IDeadOrAliveExecutor';
 import { Superposition } from './Superposition';
 
-export class AnyExecutor<S, F extends Error, T = S, E extends Error = F>
-  implements IDeadOrAliveExecutor<S, F, 'AnyExecutor'> {
-  public readonly noun: 'AnyExecutor' = 'AnyExecutor';
+export class MapExecutor<S, F extends Error, T = S, E extends Error = F>
+  implements IDeadOrAliveExecutor<S, F, 'MapExecutor'> {
+  public readonly noun: 'MapExecutor' = 'MapExecutor';
   private readonly alive: AliveExecutor<S, T, E>;
-  private readonly dead: DeadExecutor<T, F, E>;
+  private readonly dead: DeadNothingExecutor<F>;
 
   public static of<S, F extends Error, T = S, E extends Error = F>(
     aliveMapper: UnaryFunction<S, PromiseLike<T> | Superposition<T, E> | T>,
-    deadMapper: UnaryFunction<F, PromiseLike<T> | Superposition<T, E> | T>,
     resolve: Resolve<T>,
-    reject: Reject<E>
-  ): AnyExecutor<S, F, T, E> {
+    reject: Reject<F | E>
+  ): MapExecutor<S, F, T, E> {
     const alive: AliveExecutor<S, T, E> = AliveExecutor.of<S, T, E>(aliveMapper, resolve, reject);
-    const dead: DeadExecutor<T, F, E> = DeadExecutor.of<T, F, E>(deadMapper, resolve, reject);
+    const dead: DeadNothingExecutor<F> = DeadNothingExecutor.of<F>(reject);
 
-    return new AnyExecutor<S, F, T, E>(alive, dead);
+    return new MapExecutor<S, F, T, E>(alive, dead);
   }
 
-  protected constructor(alive: AliveExecutor<S, T, E>, dead: DeadExecutor<T, F, E>) {
+  protected constructor(alive: AliveExecutor<S, T, E>, dead: DeadNothingExecutor<F>) {
     this.alive = alive;
     this.dead = dead;
   }
