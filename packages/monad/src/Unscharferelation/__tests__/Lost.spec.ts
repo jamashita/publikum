49a7ec5890e52643e20e1783a570964855bdaaa0
jamashita <jamashita@arachnid.xyz>
2020-07-01 02:43:49 +0900

    Lost tests

---
+++ packages/monad/src/Unscharferelation/__tests__/Lost.spec.ts
@@ -0,0 +1,42 @@
+import { MockError } from '@jamashita/publikum-object';
+
+import { Lost } from '../Lost';
+
+describe('Lost', () => {
+  describe('get', () => {
+    it('throws given error', () => {
+      const error1: MockError = new MockError();
+      const error2: MockError = new MockError();
+      const lost1: Lost<void> = Lost.of<void>(error1);
+      const lost2: Lost<number> = Lost.of<number>(error2);
+
+      expect(() => {
+        lost1.get();
+      }).toThrow(error1);
+      expect(() => {
+        lost2.get();
+      }).toThrow(error2);
+    });
+  });
+
+  describe('isPresent', () => {
+    it('returns false', () => {
+      const error: MockError = new MockError();
+      const lost1: Lost<void> = Lost.of<void>(error);
+      const lost2: Lost<number> = Lost.of<number>(error);
+
+      expect(lost1.isPresent()).toBe(false);
+      expect(lost2.isPresent()).toBe(false);
+    });
+  });
+  describe('isAbsent', () => {
+    it('returns false', () => {
+      const error: MockError = new MockError();
+      const lost1: Lost<void> = Lost.of<void>(error);
+      const lost2: Lost<number> = Lost.of<number>(error);
+
+      expect(lost1.isAbsent()).toBe(false);
+      expect(lost2.isAbsent()).toBe(false);
+    });
+  });
+});
