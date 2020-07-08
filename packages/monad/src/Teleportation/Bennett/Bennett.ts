bee3d6d5bdc225511d83ecf0ff7d4d24fa936ea1
jamashita <jamashita@arachnid.xyz>
2020-07-08 21:17:00 +0900

    new status definied

--- packages/monad/src/Teleportation/Bennett/Bennett.ts
+++ packages/monad/src/Teleportation/Bennett/Bennett.ts
@@ -2,9 +2,10 @@ import { Noun } from '@jamashita/publikum-interface';
 
 import { Cancelled } from './Cancelled';
 import { Disappeared } from './Disappeared';
+import { Failed } from './Failed';
 import { Received } from './Received';
 
-type BennettType = 'Received' | 'Disappeared' | 'Pending' | 'Cancelled';
+type BennettType = 'Received' | 'Disappeared' | 'Pending' | 'Failed' | 'Cancelled';
 
 export interface Bennett<R, N extends BennettType = BennettType> extends Noun<N> {
   readonly noun: N;
@@ -14,6 +15,8 @@ export interface Bennett<R, N extends BennettType = BennettType> extends Noun<N>
   isReceived(): this is Received<R>;
 
   isDisappeared(): this is Disappeared<R>;
+
+  isFailed(): this is Failed<R>;
 
   isCancelled(): this is Cancelled<R>;
 }
