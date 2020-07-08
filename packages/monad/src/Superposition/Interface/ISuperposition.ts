bae13b6bcd45157e94433e0d6b545effb623b74d
jamashita <jamashita@arachnid.xyz>
2020-07-08 15:36:43 +0900

    interface update

--- packages/monad/src/Superposition/Interface/ISuperposition.ts
+++ packages/monad/src/Superposition/Interface/ISuperposition.ts
@@ -4,6 +4,7 @@ import { Consumer, Predicate, UnaryFunction } from '@jamashita/publikum-type';
 import { IUnscharferelation } from '../../Unscharferelation/Interface/IUnscharferelation';
 import { SuperpositionError } from '../Error/SuperpositionError';
 import { Schrodinger } from '../Schrodinger/Schrodinger';
+import { DeadConstructor } from './DeadConstructor';
 import { Detoxicated } from './Detoxicated';
 
 export interface ISuperposition<A, D extends Error, N extends string = string> extends Noun<N> {
@@ -14,16 +15,19 @@ export interface ISuperposition<A, D extends Error, N extends string = string> e
   filter(predicate: Predicate<A>): ISuperposition<A, D | SuperpositionError>;
 
   map<B = A, E extends Error = D>(
-    mapper: UnaryFunction<Detoxicated<A>, ISuperposition<B, E> | PromiseLike<Detoxicated<B>> | Detoxicated<B>>
+    mapper: UnaryFunction<Detoxicated<A>, ISuperposition<B, E> | PromiseLike<Detoxicated<B>> | Detoxicated<B>>,
+    ...errors: Array<DeadConstructor<E>>
   ): ISuperposition<B, D | E>;
 
   recover<B = A, E extends Error = D>(
-    mapper: UnaryFunction<D, ISuperposition<B, E> | PromiseLike<Detoxicated<B>> | Detoxicated<B>>
+    mapper: UnaryFunction<D, ISuperposition<B, E> | PromiseLike<Detoxicated<B>> | Detoxicated<B>>,
+    ...errors: Array<DeadConstructor<E>>
   ): ISuperposition<A | B, E>;
 
   transform<B = A, E extends Error = D>(
     alive: UnaryFunction<Detoxicated<A>, ISuperposition<B, E> | PromiseLike<Detoxicated<B>> | Detoxicated<B>>,
-    dead: UnaryFunction<D, ISuperposition<B, E> | PromiseLike<Detoxicated<B>> | Detoxicated<B>>
+    dead: UnaryFunction<D, ISuperposition<B, E> | PromiseLike<Detoxicated<B>> | Detoxicated<B>>,
+    ...errors: Array<DeadConstructor<E>>
   ): ISuperposition<B, E>;
 
   pass(accepted: Consumer<Detoxicated<A>>, declined: Consumer<D>, thrown: Consumer<unknown>): unknown;
