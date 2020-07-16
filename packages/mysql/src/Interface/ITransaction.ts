723c14fe359c901abab6c03b55ed6329c0cb15d4
jamashita <jamashita@arachnid.xyz>
2020-07-16 22:59:16 +0900

    reverted

--- packages/mysql/src/Interface/ITransaction.ts
+++ packages/mysql/src/Interface/ITransaction.ts
@@ -3,5 +3,5 @@ import { Noun } from '@jamashita/publikum-interface';
 import { ISQL } from './ISQL';
 
 export interface ITransaction<R, N extends string = string> extends Noun<N> {
-  with(sql: ISQL): R;
+  with(sql: ISQL): Promise<R>;
 }
