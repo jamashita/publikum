44cb6dec78704a0dbe33f04d35c3f6cb53847ac7
jamashita <jamashita@arachnid.xyz>
2020-07-16 22:31:11 +0900

    ITransaction udpate

--- packages/mysql/src/Interface/ITransaction.ts
+++ packages/mysql/src/Interface/ITransaction.ts
@@ -2,6 +2,6 @@ import { Noun } from '@jamashita/publikum-interface';
 
 import { ISQL } from './ISQL';
 
-export interface ITransaction<R> extends Noun {
-  with(sql: ISQL): Promise<R>;
+export interface ITransaction<R, N extends string = string> extends Noun<N> {
+  with(sql: ISQL): R;
 }
