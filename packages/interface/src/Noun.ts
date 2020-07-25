9ed52b32f82b0aef76781fd7b8cf2e5ac91109e0
jamashita <jamashita@arachnid.xyz>
2020-07-25 22:25:54 +0900

    Noun

--- packages/interface/src/Noun.ts
+++ packages/interface/src/Noun.ts
@@ -1,3 +1,16 @@
+import { Kind } from '@jamashita/publikum-type';
+
 export interface Noun<N extends string = string> {
   readonly noun: N;
 }
+
+export const isNoun = (n: unknown): n is Noun => {
+  if (!Kind.isObject<Noun>(n)) {
+    return false;
+  }
+  if (typeof n.noun !== 'string') {
+    return false;
+  }
+
+  return true;
+};
---
+++ packages/interface/src/__tests__/Noun.spec.ts
@@ -0,0 +1,31 @@
+import { isNoun, Noun } from '../Noun';
+
+class MockNoun implements Noun {
+  public readonly noun: string = 'popo';
+}
+
+describe('Noun', () => {
+  describe('isNoun', () => {
+    it('normal case', () => {
+      expect(isNoun(null)).toBe(false);
+      expect(isNoun(undefined)).toBe(false);
+      expect(isNoun('')).toBe(false);
+      expect(isNoun('123')).toBe(false);
+      expect(isNoun('abcd')).toBe(false);
+      expect(isNoun(123)).toBe(false);
+      expect(isNoun(0)).toBe(false);
+      expect(isNoun(false)).toBe(false);
+      expect(isNoun(true)).toBe(false);
+      expect(isNoun(Symbol())).toBe(false);
+      expect(isNoun(20n)).toBe(false);
+      expect(isNoun({})).toBe(false);
+      expect(isNoun([])).toBe(false);
+      expect(
+        isNoun({
+          noun: 'pipi'
+        })
+      ).toBe(true);
+      expect(isNoun(new MockNoun())).toBe(true);
+    });
+  });
+});
