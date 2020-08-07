f450939281e887537016f82bc81dba3a45977c9d
jamashita <jamashita@arachnid.xyz>
2020-08-08 00:59:40 +0900

    setImmediate

--- packages/json/src/JSONA.ts
+++ packages/json/src/JSONA.ts
@@ -5,27 +5,27 @@ import { JSONAError } from './Error/JSONAError';
 export class JSONA {
   public static parse<T extends ObjectLiteral>(text: string): Promise<T> {
     return new Promise<T>((resolve: Resolve<T>, reject: Reject) => {
-      setTimeout(() => {
+      setImmediate(() => {
         try {
           resolve(JSON.parse(text));
         }
         catch (err) {
           reject(new JSONAError(err));
         }
-      }, 0);
+      });
     });
   }
 
   public static stringify(value: ObjectLiteral): Promise<string> {
     return new Promise<string>((resolve: Resolve<string>, reject: Reject) => {
-      setTimeout(() => {
+      setImmediate(() => {
         try {
           resolve(JSON.stringify(value));
         }
         catch (err) {
           reject(new JSONAError(err));
         }
-      }, 0);
+      });
     });
   }
 
