f9a9b346bd3dd41f0adc16784f9e1fb072feca2e
jamashita <jamashita@arachnid.xyz>
2020-09-01 13:08:34 +0900

    deleted

--- packages/monad/src/Epoque/Interface/AcceptEpoque.ts
+++
@@ -1,5 +0,0 @@
-import { Noun } from '@jamashita/publikum-interface';
-
-export interface AcceptEpoque<A, N extends string = string> extends Noun<N> {
-  accept(value: A): unknown | void;
-}
--- packages/monad/src/Epoque/Interface/DeclineEpoque.ts
+++
@@ -1,5 +0,0 @@
-import { Noun } from '@jamashita/publikum-interface';
-
-export interface DeclineEpoque<D, N extends string = string> extends Noun<N> {
-  decline(value: D): unknown | void;
-}
