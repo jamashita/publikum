cfd50b7587e6b926553c341e186779626d7e8415
jamashita <jamashita@arachnid.xyz>
2020-09-01 14:48:37 +0900

    renewd

--- packages/monad/src/Plan/Mock/MockMappingPlan.ts
+++ packages/monad/src/Plan/Mock/MockMappingPlan.ts
@@ -1,11 +1,9 @@
 import { UnimplementedError } from '@jamashita/publikum-error';
-
 import { MappingPlan } from '../Interface/MappingPlan';
 
 export class MockMappingPlan<M = void> implements MappingPlan<M, 'MockMappingPlan'> {
   public readonly noun: 'MockMappingPlan' = 'MockMappingPlan';
 
-  public onMap(value: M): never;
   public onMap(): never {
     throw new UnimplementedError();
   }
--- packages/monad/src/Plan/Mock/MockRecoveryPlan.ts
+++ packages/monad/src/Plan/Mock/MockRecoveryPlan.ts
@@ -1,11 +1,9 @@
 import { UnimplementedError } from '@jamashita/publikum-error';
-
 import { RecoveryPlan } from '../Interface/RecoveryPlan';
 
 export class MockRecoveryPlan<R = void> implements RecoveryPlan<R, 'MockRecoveryPlan'> {
   public readonly noun: 'MockRecoveryPlan' = 'MockRecoveryPlan';
 
-  public onRecover(value: R): never;
   public onRecover(): never {
     throw new UnimplementedError();
   }
