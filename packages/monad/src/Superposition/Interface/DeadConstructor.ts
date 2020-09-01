72a235f307aa49df0a1ba367a88d2946c323fdd2
jamashita <jamashita@arachnid.xyz>
2020-09-01 15:53:51 +0900

    default prramtere

--- packages/monad/src/Superposition/Interface/DeadConstructor.ts
+++ packages/monad/src/Superposition/Interface/DeadConstructor.ts
@@ -1,4 +1,4 @@
-export interface DeadConstructor<E extends Error> {
+export interface DeadConstructor<E extends Error = Error> {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   new(...args: Array<any>): E;
 }
