33220687e2e3ba7e82325a05c85c7df58fd41fab
jamashita <jamashita@arachnid.xyz>
2020-10-20 09:10:21 +0900

    feat: 🎸 Tree Definitions

---
+++ packages/tree/src/__tests__/StructurableTree.spec.ts
@@ -0,0 +1,24 @@
+import { MockTreeID } from '@jamashita/publikum-tree';
+import sinon, { SinonSpy } from 'sinon';
+import { MockTreeObject } from '../Mock/MockTreeObject';
+import { StructurableTree } from '../StructurableTree';
+import { StructurableTreeNode } from '../TreeNode/StructurableTreeNode';
+
+describe('StructurableTree', () => {
+  describe('getTreeID', () => {
+    it('delegates its root instance', () => {
+      expect.assertions(1);
+
+      const spy: SinonSpy = sinon.spy();
+      const root: StructurableTreeNode<MockTreeID, MockTreeObject<MockTreeID>> = StructurableTreeNode.of<MockTreeID, MockTreeObject<MockTreeID>>(new MockTreeObject<MockTreeID>(new MockTreeID('mock')));
+
+      root.getTreeID = spy;
+
+      const tree: StructurableTree<MockTreeID, MockTreeObject<MockTreeID>> = StructurableTree.of<MockTreeID, MockTreeObject<MockTreeID>>(root);
+
+      tree.getTreeID();
+
+      expect(spy.called).toBe(true);
+    });
+  });
+});
