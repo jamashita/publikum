8752f2e0bb823bc98cb3baadadac81e6286be20a
jamashita <jamashita@arachnid.xyz>
2020-10-23 16:53:26 +0900

    feat: 🎸 ReadonlyTreeNode

--- packages/tree/src/TreeNode/Interface/TreeNode.ts
+++ packages/tree/src/TreeNode/Interface/TreeNode.ts
@@ -1,10 +1,5 @@
-import { ReadonlyAddress } from '@jamashita/publikum-collection';
-import { Nominative } from '@jamashita/publikum-interface';
+import { ReadonlyTreeNode } from './ReadonlyTreeNode';
 
-export interface TreeNode<V, T extends TreeNode<V, T>, N extends string = string> extends Nominative<N> {
-  getValue(): V;
-
-  getChildren(): ReadonlyAddress<T>;
-
-  isLeaf(): boolean;
+export interface TreeNode<V, T extends TreeNode<V, T>, N extends string = string> extends ReadonlyTreeNode<V, T, N> {
+  set(value: V): void;
 }
