b6b1d05bc11140b83754acbc4dc81a67cedd575b
jamashita <jamashita@arachnid.xyz>
2020-10-25 00:25:03 +0900

    test: 💍 udpate

--- packages/tree/src/StructurableTree.ts
+++ packages/tree/src/StructurableTree.ts
@@ -1,15 +1,17 @@
-import { ATree } from './Abstract/ATree';
 import { StructurableTreeObject } from './Interface/StructurableTreeObject';
 import { TreeID } from './Interface/TreeID';
+import { Tree } from './Tree';
 import { StructurableTreeNode } from './TreeNode/StructurableTreeNode';
 
-export class StructurableTree<K extends TreeID, V extends StructurableTreeObject<K>> extends ATree<V, StructurableTreeNode<K, V>, 'StructurableTree'> {
+export class StructurableTree<K extends TreeID, V extends StructurableTreeObject<K>> extends Tree<V, StructurableTreeNode<K, V>, 'StructurableTree'> {
+  public readonly noun: 'StructurableTree' = 'StructurableTree';
+
   public static of<KT extends TreeID, VT extends StructurableTreeObject<KT>>(root: StructurableTreeNode<KT, VT>): StructurableTree<KT, VT> {
     return new StructurableTree<KT, VT>(root);
   }
 
   protected constructor(root: StructurableTreeNode<K, V>) {
-    super(root, 'StructurableTree');
+    super(root);
   }
 
   public getTreeID(): K {
