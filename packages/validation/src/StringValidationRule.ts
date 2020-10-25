f9c91c349016afb6b7ea729b129c967a3dad5263
jamashita <jamashita@arachnid.xyz>
2020-10-25 17:29:51 +0900

    refactor: 💡 parameter supply method changed

--- packages/validation/src/StringValidationRule.ts
+++ packages/validation/src/StringValidationRule.ts
@@ -9,28 +9,32 @@ export type StringValidatorArgs = Partial<Readonly<{
 }>>;
 
 export class StringValidationRule implements ValidationRule {
-  private readonly args: StringValidatorArgs;
+  private readonly min?: number;
+  private readonly max?: number;
+  private readonly pattern?: RegExp;
 
-  public constructor(args: StringValidatorArgs) {
-    this.args = args;
+  public constructor({ min, max, pattern }: StringValidatorArgs) {
+    this.min = min;
+    this.max = max;
+    this.pattern = pattern;
   }
 
   public evaluate(_target: object, value: unknown): void {
     if (!Kind.isString(value)) {
       throw new ValidationError(`VALUE IS NOT STRING. GIVEN: ${Kind.notate(value)}`);
     }
-    if (!Kind.isUndefined(this.args.min)) {
-      if (value.length < this.args.min) {
+    if (!Kind.isUndefined(this.min)) {
+      if (value.length < this.min) {
         throw new ValidationError(`VALUE IS SHORTER THAN min. GIVEN: ${value}`);
       }
     }
-    if (!Kind.isUndefined(this.args.max)) {
-      if (this.args.max < value.length) {
+    if (!Kind.isUndefined(this.max)) {
+      if (this.max < value.length) {
         throw new ValidationError(`VALUE IS LONGER THAN max. GIVEN: ${value}`);
       }
     }
-    if (!Kind.isUndefined(this.args.pattern)) {
-      if (!this.args.pattern.test(value)) {
+    if (!Kind.isUndefined(this.pattern)) {
+      if (!this.pattern.test(value)) {
         throw new ValidationError(`VALUE DOES NOT FOLLOW THE PATTERN. GIVEN: ${value}`);
       }
     }
