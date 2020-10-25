c92f4f2c5306b4ad62d4ca66c46c3bd9315e4600
jamashita <jamashita@arachnid.xyz>
2020-10-25 16:55:25 +0900

    refactor: 💡 moved

--- packages/validation/src/Decorator/Validate.ts
+++ packages/validation/src/Decorator/Validate.ts
@@ -1,6 +1,27 @@
-import { Kind } from '@jamashita/publikum-type';
+import { Ambiguous, Kind } from '@jamashita/publikum-type';
 import { ValidationRule } from '../Interface/ValidationRule';
-import { getKeys, getRules } from '../Validator';
+
+const VALIDATION_KEY: symbol = Symbol();
+
+const getRules = (target: object, key: string | symbol): Array<ValidationRule> => {
+  const rules: Ambiguous<Array<ValidationRule>> = Reflect.getMetadata(VALIDATION_KEY, target, key) as Ambiguous<Array<ValidationRule>>;
+
+  if (Kind.isUndefined(rules)) {
+    return [];
+  }
+
+  return rules;
+};
+
+const getKeys = (target: object): Set<string | symbol> => {
+  const properties: Ambiguous<Set<string | symbol>> = Reflect.getMetadata(VALIDATION_KEY, target) as Ambiguous<Set<string | symbol>>;
+
+  if (Kind.isUndefined(properties)) {
+    return new Set<string | symbol>();
+  }
+
+  return properties;
+};
 
 export const Validate = (): MethodDecorator => {
   return <T>(target: object, _k: string | symbol, descriptor: TypedPropertyDescriptor<T>): void => {
@@ -22,4 +43,18 @@ export const Validate = (): MethodDecorator => {
       });
     });
   };
+};
+
+export const addRule = (target: object, key: string | symbol, rule: ValidationRule): void => {
+  const rules: Array<ValidationRule> = getRules(target, key);
+  const properties: Set<string | symbol> = getKeys(target);
+
+  rules.push(rule);
+
+  if (!properties.has(key)) {
+    properties.add(key);
+  }
+
+  Reflect.defineMetadata(VALIDATION_KEY, rules, target, key);
+  Reflect.defineMetadata(VALIDATION_KEY, properties, target);
 };
