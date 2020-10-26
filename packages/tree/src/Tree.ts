1f639db22714fcf4bd56ff752b5ca9403bc5fd44
jamashita <jamashita@arachnid.xyz>
2020-10-27 00:14:09 +0900

    test: 💍 omittable generic

--- packages/tree/src/Tree.ts
+++ packages/tree/src/Tree.ts
@@ -3,7 +3,7 @@ import { Objet } from '@jamashita/publikum-object';
 import { Nullable, Predicate } from '@jamashita/publikum-type';
 import { TreeNode } from './TreeNode/TreeNode';
 
-export abstract class Tree<V extends Nominative, T extends TreeNode<V, T>, N extends string> extends Objet<N> {
+export abstract class Tree<V extends Nominative, T extends TreeNode<V, T>, N extends string = string> extends Objet<N> {
   protected readonly root: T;
 
   protected constructor(root: T) {
