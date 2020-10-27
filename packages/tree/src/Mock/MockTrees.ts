710ca49bd6415056ba6af6b71ac473c668d797e7
jamashita <jamashita@arachnid.xyz>
2020-10-27 22:39:23 +0900

    feat: 🎸 Abstract class Trees

--- packages/tree/src/Mock/MockTrees.ts
+++ packages/tree/src/Mock/MockTrees.ts
@@ -1,11 +1,11 @@
-import { ImmutableProject } from '@jamashita/publikum-collection';
-import { MockTree, MockTreeNode, StructurableTreeNode, TreeID } from '@jamashita/publikum-tree';
+import { ReadonlyProject } from '@jamashita/publikum-collection';
+import { MockTree, MockTreeNode, MockTreeObject, TreeID } from '@jamashita/publikum-tree';
 import { Trees } from '../Trees';
 
-export class MockTrees<K extends TreeID, V extends StructurableTreeNode<K, V>> extends Trees<K, V, MockTreeNode<K, V>, MockTree<K, V>, 'MockTrees'> {
+export class MockTrees<K extends TreeID, V extends MockTreeObject<K>> extends Trees<K, V, MockTreeNode<K, V>, MockTree<K, V>, 'MockTrees'> {
   public readonly noun: 'MockTrees' = 'MockTrees';
 
-  public constructor(trees: ImmutableProject<K, MockTree<K, V>>) {
+  public constructor(trees: ReadonlyProject<K, MockTree<K, V>>) {
     super(trees);
   }
 }
