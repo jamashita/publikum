4a375ce2a3da7b8d05cf4bcaba59c31b979cb8a4
jamashita <jamashita@arachnid.xyz>
2020-10-29 01:09:57 +0900

    fix: 🐛 expose path correction

--- packages/tree/src/ClosureTable/Mock/index.ts
+++ packages/tree/src/ClosureTable/Mock/index.ts
@@ -1,5 +1,4 @@
 export * from './MockClosureTable';
 export * from './MockClosureTableHierarchies';
 export * from './MockClosureTableHierarchy';
-export * from './MockClosureTableOffsprings';
 export * from './MockTreeIDFactory';
--- packages/tree/src/TreeNode/index.ts
+++ packages/tree/src/TreeNode/index.ts
@@ -1,4 +1,4 @@
-export * from './TreeNode';
 export * from './Mock/MockTreeNode';
 export * from './SerializableTreeNode';
 export * from './StructurableTreeNode';
+export * from './TreeNode';
