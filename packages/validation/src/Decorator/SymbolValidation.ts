d1eeffa4af8b5cd21fdd9c34122bf351edabf906
jamashita <jamashita@arachnid.xyz>
2020-10-30 14:09:05 +0900

    feat: 🎸 factory and sintleton

--- packages/validation/src/Decorator/SymbolValidation.ts
+++ packages/validation/src/Decorator/SymbolValidation.ts
@@ -2,7 +2,7 @@ import { SymbolValidationRule } from '../SymbolValidationRule';
 import { addRule } from './Validate';
 
 export const SymbolValidation = (): ParameterDecorator => {
-  const v: SymbolValidationRule = new SymbolValidationRule();
+  const v: SymbolValidationRule = SymbolValidationRule.of();
 
   return (target: object, key: string | symbol, index: number) => {
     addRule(target, key, index, v);
