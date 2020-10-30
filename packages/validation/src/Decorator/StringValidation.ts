f11f36528dfc4855a186a5eca30ab0f15abe8008
jamashita <jamashita@arachnid.xyz>
2020-10-30 15:57:57 +0900

    feat: 🎸 factory and new validations

--- packages/validation/src/Decorator/StringValidation.ts
+++ packages/validation/src/Decorator/StringValidation.ts
@@ -1,8 +1,8 @@
 import { StringValidationArgs, StringValidationRule } from '../StringValidationRule';
 import { addRule } from './Validate';
 
-export const StringValidation = (args: StringValidationArgs = {}): ParameterDecorator => {
-  const v: StringValidationRule = new StringValidationRule(args);
+export const StringValidation = (args?: StringValidationArgs): ParameterDecorator => {
+  const v: StringValidationRule = StringValidationRule.of(args);
 
   return (target: object, key: string | symbol, index: number) => {
     addRule(target, key, index, v);
--- packages/validation/src/Decorator/__tests__/StringValidation.spec.ts
+++ packages/validation/src/Decorator/__tests__/StringValidation.spec.ts
@@ -12,20 +12,47 @@ class MockValidation {
   }
 
   @Validate()
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  public act2(@StringValidation({ min: 4 }) _s: unknown): void {
+  public act2(@StringValidation({
+    type: 'numerical'
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  }) _s: unknown): void {
     // NOOP
   }
 
   @Validate()
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  public act3(@StringValidation({ max: 4 }) _s: unknown): void {
+  public act3(@StringValidation({
+    type: 'pattern',
+    pattern: /^a.*b$/iu
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  }) _s: unknown): void {
     // NOOP
   }
 
   @Validate()
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  public act4(@StringValidation({ pattern: /^a.*b$/iu }) _s: unknown): void {
+  public act4(@StringValidation({
+    type: 'length',
+    min: 4
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  }) _s: unknown): void {
+    // NOOP
+  }
+
+  @Validate()
+  public act5(@StringValidation({
+    type: 'length',
+    max: 4
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  }) _s: unknown): void {
+    // NOOP
+  }
+
+  @Validate()
+  public act6(@StringValidation({
+    type: 'length',
+    min: 4,
+    max: 6
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  }) _s: unknown): void {
     // NOOP
   }
 }
@@ -79,25 +106,103 @@ describe('StringValidation', () => {
       }).toThrow(ValidationError);
     });
 
+    it('does not throw any Error when given string can be converted to number', () => {
+      expect.assertions(4);
+
+      const validation: MockValidation = new MockValidation();
+
+      expect(() => {
+        validation.act2('123');
+      }).not.toThrow(ValidationError);
+      expect(() => {
+        validation.act2('-123');
+      }).not.toThrow(ValidationError);
+      expect(() => {
+        validation.act2('0');
+      }).not.toThrow(ValidationError);
+      expect(() => {
+        validation.act2('0.18');
+      }).not.toThrow(ValidationError);
+    });
+
+    it('throws ValidationError when given string cannot be converted to number', () => {
+      expect.assertions(7);
+
+      const validation: MockValidation = new MockValidation();
+
+      expect(() => {
+        validation.act2('1.2.3');
+      }).toThrow(ValidationError);
+      expect(() => {
+        validation.act2('0..');
+      }).toThrow(ValidationError);
+      expect(() => {
+        validation.act2('0..10');
+      }).toThrow(ValidationError);
+      expect(() => {
+        validation.act2('a');
+      }).toThrow(ValidationError);
+      expect(() => {
+        validation.act2('a');
+      }).toThrow(ValidationError);
+      expect(() => {
+        validation.act2('-Infinity');
+      }).toThrow(ValidationError);
+      expect(() => {
+        validation.act2('NaN');
+      }).toThrow(ValidationError);
+    });
+
+    it('throws ValidationError when string pattern does not match', () => {
+      expect.assertions(8);
+
+      const validation: MockValidation = new MockValidation();
+
+      expect(() => {
+        validation.act3('a');
+      }).toThrow(ValidationError);
+      expect(() => {
+        validation.act3('b');
+      }).toThrow(ValidationError);
+      expect(() => {
+        validation.act3('ab');
+      }).not.toThrow(ValidationError);
+      expect(() => {
+        validation.act3('ba');
+      }).toThrow(ValidationError);
+      expect(() => {
+        validation.act3('aab');
+      }).not.toThrow(ValidationError);
+      expect(() => {
+        validation.act3('abb');
+      }).not.toThrow(ValidationError);
+      expect(() => {
+        validation.act3('acb');
+      }).not.toThrow(ValidationError);
+      expect(() => {
+        validation.act3('abcab');
+      }).not.toThrow(ValidationError);
+    });
+
     it('throws ValidationError when less than min string length given', () => {
       expect.assertions(5);
 
       const validation: MockValidation = new MockValidation();
 
       expect(() => {
-        validation.act2('');
+        validation.act4('');
       }).toThrow(ValidationError);
       expect(() => {
-        validation.act2('p');
+        validation.act4('p');
       }).toThrow(ValidationError);
       expect(() => {
-        validation.act2('pq');
+        validation.act4('pq');
       }).toThrow(ValidationError);
       expect(() => {
-        validation.act2('pqw');
+        validation.act4('pqw');
       }).toThrow(ValidationError);
       expect(() => {
-        validation.act2('pqwo');
+        validation.act4('pqwo');
       }).not.toThrow(ValidationError);
     });
 
@@ -107,51 +212,48 @@ describe('StringValidation', () => {
       const validation: MockValidation = new MockValidation();
 
       expect(() => {
-        validation.act3('pqwo1029');
+        validation.act5('pqwo1029');
       }).toThrow(ValidationError);
       expect(() => {
-        validation.act3('pqwo102');
+        validation.act5('pqwo102');
       }).toThrow(ValidationError);
       expect(() => {
-        validation.act3('pqwo10');
+        validation.act5('pqwo10');
       }).toThrow(ValidationError);
       expect(() => {
-        validation.act3('pqwo1');
+        validation.act5('pqwo1');
       }).toThrow(ValidationError);
       expect(() => {
-        validation.act3('pqwo');
+        validation.act5('pqwo');
       }).not.toThrow(ValidationError);
     });
 
-    it('throws ValidationError when string pattern does not match', () => {
-      expect.assertions(8);
+    it('throws ValidationError when given value is less than min and greater than max string length given', () => {
+      expect.assertions(7);
 
       const validation: MockValidation = new MockValidation();
 
       expect(() => {
-        validation.act4('a');
+        validation.act6('pq');
       }).toThrow(ValidationError);
       expect(() => {
-        validation.act4('b');
+        validation.act6('pqw');
       }).toThrow(ValidationError);
       expect(() => {
-        validation.act4('ab');
+        validation.act6('pqwo');
       }).not.toThrow(ValidationError);
       expect(() => {
-        validation.act4('ba');
+        validation.act6('pqwo1');
+      }).not.toThrow(ValidationError);
+      expect(() => {
+        validation.act6('pqwo10');
+      }).not.toThrow(ValidationError);
+      expect(() => {
+        validation.act6('pqwo102');
       }).toThrow(ValidationError);
       expect(() => {
-        validation.act4('aab');
-      }).not.toThrow(ValidationError);
-      expect(() => {
-        validation.act4('abb');
-      }).not.toThrow(ValidationError);
-      expect(() => {
-        validation.act4('acb');
-      }).not.toThrow(ValidationError);
-      expect(() => {
-        validation.act4('abcab');
-      }).not.toThrow(ValidationError);
+        validation.act6('pqwo1029');
+      }).toThrow(ValidationError);
     });
   });
 });
