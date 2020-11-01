dcd11bde77da5f65084d61a3003ed4203780d2a2
jamashita <jamashita@arachnid.xyz>
2020-11-02 00:03:52 +0900

    feat: 🎸 made it interface and made a new abstract class

--- packages/tree/src/Tree.ts
+++ packages/tree/src/Tree.ts
@@ -1,44 +1,15 @@
 import { Nominative } from '@jamashita/publikum-interface';
-import { Objet } from '@jamashita/publikum-object';
 import { Nullable, Predicate } from '@jamashita/publikum-type';
 import { TreeNode } from './TreeNode/TreeNode';
 
-export abstract class Tree<V extends Nominative, T extends TreeNode<V, T>, N extends string = string> extends Objet<N> {
-  protected readonly root: T;
+export interface Tree<V extends Nominative, T extends TreeNode<V, T>, N extends string = string> extends Nominative<N> {
+  getRoot(): T;
 
-  protected constructor(root: T) {
-    super();
-    this.root = root;
-  }
+  contains(value: V): boolean;
 
-  public equals(other: unknown): boolean {
-    if (this === other) {
-      return true;
-    }
-    if (!(other instanceof Tree)) {
-      return false;
-    }
+  find(predicate: Predicate<V>): Nullable<T>;
 
-    return this.root.equals(other.root);
-  }
+  size(): number;
 
-  public serialize(): string {
-    return this.root.toString();
-  }
-
-  public getRoot(): T {
-    return this.root;
-  }
-
-  public contains(value: V): boolean {
-    return this.root.contains(value);
-  }
-
-  public find(predicate: Predicate<V>): Nullable<T> {
-    return this.root.find(predicate);
-  }
-
-  public size(): number {
-    return this.root.size();
-  }
+  values(): Iterable<V>;
 }
