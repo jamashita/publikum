5df4d69423c0e5aeaca00391e181ce86632ad929
jamashita <jamashita@arachnid.xyz>
2020-12-07 10:13:52 +0900

    feat: 🎸 entity udpate

--- packages/object/src/Entity.ts
+++ packages/object/src/Entity.ts
@@ -1,7 +1,8 @@
-import { Cloneable, Nominative } from '@jamashita/publikum-interface';
+import { Cloneable } from '@jamashita/publikum-interface';
+import { Kind } from '@jamashita/publikum-type';
 import { Objet } from './Objet';
 
-export abstract class Entity<T extends Entity<T, I, N>, I extends Nominative<N>, N extends string = string> extends Objet<N> implements Cloneable<T> {
+export abstract class Entity<I, T extends Entity<I, T, N>, N extends string = string> extends Objet<N> implements Cloneable<T> {
   public abstract getIdentifier(): I;
 
   public abstract duplicate(): T;
@@ -15,11 +16,20 @@ export abstract class Entity<T extends Entity<T, I, N>, I extends Nominative<N>,
     if (!(other instanceof Entity)) {
       return false;
     }
+    if (this.hashCode() === other.hashCode()) {
+      return true;
+    }
 
-    return this.getIdentifier().equals(other.getIdentifier());
+    return false;
   }
 
   public hashCode(): string {
-    return this.getIdentifier().hashCode();
+    const hash: I | string = this.hashor<I>(this.getIdentifier());
+
+    if (Kind.isString(hash)) {
+      return hash;
+    }
+
+    return Objet.identify(hash);
   }
 }
