56127f7e7b531af066f051b6cd449deab009c8d7
jamashita <jamashita@arachnid.xyz>
2020-12-31 22:48:00 +0900

    refactor: 💡 implememnted Iterable

--- packages/error/src/Errors.ts
+++ packages/error/src/Errors.ts
@@ -1,8 +1,9 @@
 import { Ambiguous, Kind } from '@jamashita/publikum-type';
 import { RuntimeError } from './RuntimeError';
 
-export abstract class Errors<N extends string = string> extends RuntimeError<N> {
-  private readonly errors: Array<Error>;
+export class Errors<E extends Error> extends RuntimeError<'Errors'> implements Iterable<E> {
+  public readonly noun: 'Errors' = 'Errors';
+  private readonly errors: Array<E>;
 
   private static getMessage(errors: Array<Error>): string {
     return errors.map<string>((error: Error) => {
@@ -10,13 +11,21 @@ export abstract class Errors<N extends string = string> extends RuntimeError<N> 
     }).join('\n');
   }
 
-  protected constructor(errors: Array<Error>) {
+  public constructor(errors: Array<E>) {
     super(Errors.getMessage(errors));
     this.errors = errors;
   }
 
+  public [Symbol.iterator](): Iterator<E> {
+    return this.errors[Symbol.iterator]();
+  }
+
   public getStack(): string {
-    return this.errors.map<Ambiguous<string>>((error: Error) => {
+    return this.errors.map<Ambiguous<string>>((error: E) => {
+      if (error instanceof RuntimeError) {
+        return error.getStack();
+      }
+
       return error.stack;
     }).filter((stack: Ambiguous<string>): stack is string => {
       return !Kind.isUndefined(stack);
